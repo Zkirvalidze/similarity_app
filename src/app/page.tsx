ae88270cae1a680ee6cd2e8d20f5b554af491d99
zKirvalidze <geostreamer@gmail.com>
2023-03-10 23:43:56 +0400

    reusabble header , added layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
-import Paragraph from './components/ui/Paragraph'
+import Paragraph from '../components/ui/Paragraph'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-   <main className='bg-red-400' ><Paragraph /></main>
+   <main className='bg-red-400' >hello</main>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface NavbarProps {
+    
+}
+ 
+const Navbar: FC<NavbarProps> = ({}) => {
+    return <div>navbar</div>;
+}
+ 
+export default Navbar;
