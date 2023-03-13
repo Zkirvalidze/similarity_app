91b2e158295e52a313bda88c933f99af41a4d7cf
zKirvalidze <geostreamer@gmail.com>
2023-03-13 19:11:33 +0400

    styling documentation page, added  tabs logic

---
+++ src/components/DocumentationTabs.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface DocumentationTabsProps {}
+
+const DocumentationTabs: FC<DocumentationTabsProps> = () => {
+  return <div>doc</div>;
+};
+
+export default DocumentationTabs;
